12d4c952524fec8dc974101cec2b3952091eb629
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-26 11:24:13 -0500

    DELETE on button click

--- public/script.js
+++ public/script.js
@@ -1,34 +1,49 @@
 /* eslint-disable no-console */
 "use strict";
 
+//TODO(adam): handle completed tasks
+
 $(() => {
-  const API_URL = "https://cruddy-todo.firebaseio.com/task.json";
+  const API_URL = "https://cruddy-todo.firebaseio.com/task";
 
   //NOTE(adam): READ:   GET data from firebase and display in table
-  $.get(API_URL).done((data) => {
+  $.get(`${API_URL}.json`).done((data) => {
     if(data) {
-      Object.keys(data).forEach((key) => addItemToTable(data[key]));
+      Object.keys(data).forEach((id) => addItemToTable(data[id], id));
     }
   });
 
   //TODO(adam): CREATE: form submit event to POST data to firebase
   $("form").submit(() => {
     //TODO(adam): grab the form text
-    $.post(API_URL, JSON.stringify({task: "I was posted"}));
+    $.post(`${API_URL}.json`, JSON.stringify({task: "I was posted"}));
     //TODO(adam): make this not refresh the page
-  })
+  });
+
+  //NOTE(adam): DELETE: click event on delete to send DELETE to firebase
+  //NOTE(adam): dynamic click event on delete buttons
+  $("tbody").on("click", ".delete", (e) => {
+    const $row = $(e.target).closest("tr");
+    const id = $row.data("id");
+    $.ajax({
+      url: `${API_URL}/${id}.json`,
+      type: "DELETE"
+    }).done(function() {
+      $row.remove();
+    });
+  });
 
 });
 
-//TODO(adam): DELETE: click event on delete to send DELETE to firebase
 //TODO(adam): UPDATE: click event on complete to send PUT/PATCH to firebase
 
-function addItemToTable(item) {
-  const row = `<tr>
+//NOTE(adam): id as second means it would still show up just not have id
+function addItemToTable(item, id) {
+  const row = `<tr data-id="${id}">
     <td>${item.task}</td>
     <td>
       <button class="btn btn-success">Complete</button>
-      <button class="btn btn-danger">Delete</button>
+      <button class="delete btn btn-danger">Delete</button>
     </td>
   </tr>`;
 
