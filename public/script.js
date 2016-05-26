1f8c10d719123c19a2cab82e8efb0ef4683648c7
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-26 11:01:28 -0500

    Post method of form submit

--- public/script.js
+++ public/script.js
@@ -4,16 +4,24 @@
 $(() => {
   const API_URL = "https://cruddy-todo.firebaseio.com/task.json";
 
-  $.get(API_URL)
-    .done((data) => {
+  //NOTE(adam): READ:   GET data from firebase and display in table
+  $.get(API_URL).done((data) => {
+    if(data) {
       Object.keys(data).forEach((key) => addItemToTable(data[key]));
-    });
-})
+    }
+  });
 
-//TODO(adam): CREATE: form submit event to POST data to firebase
-//TODO(adam): READ:   GET data from firebase and display in table
-//TODO(adam): UPDATE: click event on complete to send PUT/PATCH to firebase
+  //TODO(adam): CREATE: form submit event to POST data to firebase
+  $("form").submit(() => {
+    //TODO(adam): grab the form text
+    $.post(API_URL, JSON.stringify({task: "I was posted"}));
+    //TODO(adam): make this not refresh the page
+  })
+
+});
+
 //TODO(adam): DELETE: click event on delete to send DELETE to firebase
+//TODO(adam): UPDATE: click event on complete to send PUT/PATCH to firebase
 
 function addItemToTable(item) {
   const row = `<tr>
